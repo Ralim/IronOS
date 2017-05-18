0f3bb4c35641aaaa685815c1aa66d4f67596ef75
Ben V. Brown <Ralim@Ralimtek.com>
2017-05-18 21:20:57 +1000

    Bump version 1.04

--- workspace/ts100/src/Main.c
+++ workspace/ts100/src/Main.c
@@ -37,7 +37,7 @@ void setup() {
 	readIronTemp(systemSettings.tempCalibration, 0,0); 	//load the default calibration value
 	Init_Oled(systemSettings.flipDisplay); 				//Init the OLED display
 
-	OLED_DrawString("VER 1.03", 8); 					//1.settings version as of current
+	OLED_DrawString("VER 1.04", 8); 					//
 	delayMs(800);										//Pause to show version number
 	Start_Watchdog(1000); 								//start the system watch dog as 1 second timeout
 }
