dcbb5bc21d1d7f1821e5fc1ad6d14026013e3533
Ben V. Brown <Ralim@Ralimtek.com>
2017-07-22 14:12:18 +1000

    Inital C# app. Reads and re-scales images well. Needs saving

---
+++ Logo GUI/TS100 Logo Editor/TS100 Logo Editor/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace TS100_Logo_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.btnSaveHex = new System.Windows.Forms.Button();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.gbScaling = new System.Windows.Forms.GroupBox();
+            this.rbScaleFit = new System.Windows.Forms.RadioButton();
+            this.rbScaleStretch = new System.Windows.Forms.RadioButton();
+            this.cbInvertImage = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.gbScaling.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLoadImage);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 141);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "1. Load source Image";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbInvertImage);
+            this.groupBox2.Controls.Add(this.gbScaling);
+            this.groupBox2.Controls.Add(this.pbImage);
+            this.groupBox2.Location = new System.Drawing.Point(220, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 141);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "2. Adjust image";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnSaveHex);
+            this.groupBox3.Location = new System.Drawing.Point(427, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 141);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "3. Export";
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(7, 20);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(187, 23);
+            this.btnLoadImage.TabIndex = 0;
+            this.btnLoadImage.Text = "Load Image";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // btnSaveHex
+            // 
+            this.btnSaveHex.Location = new System.Drawing.Point(7, 20);
+            this.btnSaveHex.Name = "btnSaveHex";
+            this.btnSaveHex.Size = new System.Drawing.Size(187, 23);
+            this.btnSaveHex.TabIndex = 0;
+            this.btnSaveHex.Text = "Save DFU File";
+            this.btnSaveHex.UseVisualStyleBackColor = true;
+            this.btnSaveHex.Click += new System.EventHandler(this.btnSaveHex_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.BackColor = System.Drawing.Color.Black;
+            this.pbImage.Location = new System.Drawing.Point(3, 20);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(192, 32);
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            // 
+            // gbScaling
+            // 
+            this.gbScaling.Controls.Add(this.rbScaleStretch);
+            this.gbScaling.Controls.Add(this.rbScaleFit);
+            this.gbScaling.Location = new System.Drawing.Point(6, 59);
+            this.gbScaling.Name = "gbScaling";
+            this.gbScaling.Size = new System.Drawing.Size(80, 76);
+            this.gbScaling.TabIndex = 1;
+            this.gbScaling.TabStop = false;
+            this.gbScaling.Text = "Scaling";
+            // 
+            // rbScaleFit
+            // 
+            this.rbScaleFit.AutoSize = true;
+            this.rbScaleFit.Checked = true;
+            this.rbScaleFit.Location = new System.Drawing.Point(7, 20);
+            this.rbScaleFit.Name = "rbScaleFit";
+            this.rbScaleFit.Size = new System.Drawing.Size(36, 17);
+            this.rbScaleFit.TabIndex = 0;
+            this.rbScaleFit.TabStop = true;
+            this.rbScaleFit.Text = "Fit";
+            this.rbScaleFit.UseVisualStyleBackColor = true;
+            this.rbScaleFit.CheckedChanged += new System.EventHandler(this.rbScaleFit_CheckedChanged);
+            // 
+            // rbScaleStretch
+            // 
+            this.rbScaleStretch.AutoSize = true;
+            this.rbScaleStretch.Location = new System.Drawing.Point(7, 44);
+            this.rbScaleStretch.Name = "rbScaleStretch";
+            this.rbScaleStretch.Size = new System.Drawing.Size(59, 17);
+            this.rbScaleStretch.TabIndex = 1;
+            this.rbScaleStretch.Text = "Stretch";
+            this.rbScaleStretch.UseVisualStyleBackColor = true;
+            this.rbScaleStretch.CheckedChanged += new System.EventHandler(this.rbScaleStretch_CheckedChanged);
+            // 
+            // cbInvertImage
+            // 
+            this.cbInvertImage.AutoSize = true;
+            this.cbInvertImage.Location = new System.Drawing.Point(92, 79);
+            this.cbInvertImage.Name = "cbInvertImage";
+            this.cbInvertImage.Size = new System.Drawing.Size(53, 17);
+            this.cbInvertImage.TabIndex = 2;
+            this.cbInvertImage.Text = "Invert";
+            this.cbInvertImage.UseVisualStyleBackColor = true;
+            this.cbInvertImage.CheckedChanged += new System.EventHandler(this.cbInvertImage_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 172);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TS100 Custom Logo editor";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.gbScaling.ResumeLayout(false);
+            this.gbScaling.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox cbInvertImage;
+        private System.Windows.Forms.GroupBox gbScaling;
+        private System.Windows.Forms.RadioButton rbScaleStretch;
+        private System.Windows.Forms.RadioButton rbScaleFit;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnSaveHex;
+    }
+}
+
---
+++ Logo GUI/TS100 Logo Editor/TS100 Logo Editor/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TS100_Logo_Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Image sourceImage;
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            //load in an image
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Select Image";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                //user has selected an image
+                string filename = dlg.FileName;
+                sourceImage = Image.FromFile(filename);
+                reDrawPreview();
+            }
+
+        }
+
+        private void reDrawPreview()
+        {
+            Bitmap b = new Bitmap(96, 16);
+            //scale mode
+            if (rbScaleFit.Checked)
+            {
+                //fit
+                float scalefactor = Math.Min((float)b.Width / (float)sourceImage.Width, (float)b.Height / (float)sourceImage.Height);
+                using (Graphics g = Graphics.FromImage(b))
+                {
+                    g.DrawImage(sourceImage, new RectangleF(0, 0, sourceImage.Width * scalefactor, sourceImage.Height * scalefactor));
+                }
+            }
+            else
+            {
+                //draw image stretched
+                using (Graphics g = Graphics.FromImage(b))
+                {
+                    g.DrawImage(sourceImage, new RectangleF(0, 0, 96, 16));
+                }
+            }
+            //We now have our downsampled colour image
+            //apply inversion
+            if (cbInvertImage.Checked)
+            {
+                for (int y = 0; (y <= (b.Height - 1)); y++)
+                {
+                    for (int x = 0; (x <= (b.Width - 1)); x++)
+                    {
+                        Color inv = b.GetPixel(x, y);
+                        inv = Color.FromArgb(255, (255 - inv.R), (255 - inv.G), (255 - inv.B));
+                        b.SetPixel(x, y, inv);
+                    }
+                }
+            }
+
+            //Threshold image
+            b = GrayScale(b);
+            //draw image at 2x scale
+            Bitmap bBig = new Bitmap(96 * 2, 16 * 2);
+            using (Graphics g = Graphics.FromImage(bBig))
+            {
+                g.DrawImage(b, new RectangleF(0, 0, bBig.Width, bBig.Height));
+            }
+            pbImage.Image = bBig;
+        }
+        public Bitmap GrayScale(Bitmap Bmp)
+        {
+            int rgb;
+            Color c;
+
+            for (int y = 0; y < Bmp.Height; y++)
+                for (int x = 0; x < Bmp.Width; x++)
+                {
+                    c = Bmp.GetPixel(x, y);
+                    rgb = (int)((c.R + c.G + c.B) / 3);
+                    if (rgb > 128)
+                        rgb = 0xFF;
+                    else
+                        rgb = 0x00;
+                    Bmp.SetPixel(x, y, Color.FromArgb(rgb, rgb, rgb));
+                }
+            return Bmp;
+        }
+
+        private void rbScaleStretch_CheckedChanged(object sender, EventArgs e)
+        {
+            reDrawPreview();
+        }
+
+        private void rbScaleFit_CheckedChanged(object sender, EventArgs e)
+        {
+            reDrawPreview();
+        }
+
+        private void cbInvertImage_CheckedChanged(object sender, EventArgs e)
+        {
+            reDrawPreview();
+        }
+
+        private void btnSaveHex_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = (Bitmap)pbImage.Image;
+            //convert image to byte array
+            byte[] data = new byte[96 * 16 / 8];
+            for (int i = 0; i < data.Length; i++)
+            {
+                //loop through all the bytes
+                byte b = 0;//local byte
+                //i sets the starting column
+                for (int y = 0; y < 8; y++)
+                {
+                    var px = bmp.GetPixel(i % 96, (i / 96) == 1 ? 8 + y : y);
+                    //we loop down the picture
+                    //LSB is the top, MSB is the bottom
+                    if (px.R >= 128)
+                    {
+                        //pixel is white
+                        b |= (byte)(1 << y);
+                    }
+                }
+                data[i] = b;
+            }
+
+        }
+    }
+}
