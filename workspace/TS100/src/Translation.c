007eb9aa37458b97e9e718a940887c1e4e6e746f
federck <33298498+federck@users.noreply.github.com>
2018-01-09 20:40:22 +0100

    [FIX] Italian translation

--- workspace/TS100/src/Translation.c
+++ workspace/TS100/src/Translation.c
@@ -331,19 +331,19 @@ const char* SettingsLongNames[16] = {
 
 const char* SettingsCalibrationWarning = "Assicurati che la punta si trovi a temperatura ambiente prima di continuare!";
 const char* SettingsResetWarning = "Ripristinare le impostazioni iniziali?";
-const char* UVLOWarningString = "DC BASSA";            // <=8 chars
-const char* UndervoltageString = "DC INSUFFICIENTE";     // <=16 chars
-const char* InputVoltageString = "V ingresso:";        // <=11 chars, preferably end with a space
-const char* WarningTipTempString = "Temp punta:";     // <=11 chars, preferably end with a space
-const char* BadTipString = "Sostituire punta";                // <=16 chars
-const char* SleepingSimpleString = "Zzz";       // Must be <= 4 chars
-const char* SleepingAdvancedString = "Standby";  // <=16 chars
-const char* WarningSimpleString = "HOT!";        // Must be <= 4 chars
-const char* WarningAdvancedString = "PUNTA CALDA!";  // <=16 chars
-const char* SleepingTipAdvancedString = "Punta:";      // <=6 chars
-const char* IdleTipString = "Punta:";     // IdleTipString+IdleSetString <= 10
-const char* IdleSetString = " Imposta:";    // preferably start with a space; IdleTipString+IdleSetString <= 10
-const char* TipDisconnectedString = "PUNTA ASSENTE"; // <=16 chars
+const char* UVLOWarningString = "DC BASSA";			// <=8 chars
+const char* UndervoltageString = "DC INSUFFICIENTE";		// <=16 chars
+const char* InputVoltageString = "V ingresso:"; 		// <=11 chars, preferably end with a space
+const char* WarningTipTempString = "Temp punta:";		// <=11 chars, preferably end with a space
+const char* BadTipString = "Sostituire punta"; 			// <=16 chars
+const char* SleepingSimpleString = "Zzz ";			// Must be <= 4 chars
+const char* SleepingAdvancedString = "Standby";			// <=16 chars
+const char* WarningSimpleString = "HOT!";			// Must be <= 4 chars
+const char* WarningAdvancedString = "PUNTA CALDA!";		// <=16 chars
+const char* SleepingTipAdvancedString = "Punta:";		// <=6 chars
+const char* IdleTipString = "Pun:";				// IdleTipString+IdleSetString <= 10
+const char* IdleSetString = " Prev:";				// preferably start with a space; IdleTipString+IdleSetString <= 10
+const char* TipDisconnectedString = "PUNTA ASSENTE"; 		// <=16 chars
 
 const char SettingRightChar = 'D';
 const char SettingLeftChar = 'S';
