cf1886395867171da7430f0c8dda7f45173a7208
Ben V. Brown <ralim@ralimtek.com>
2020-05-30 13:57:04 +1000

    Update version for release

--- workspace/TS100/version.h
+++ workspace/TS100/version.h
@@ -1,7 +1,7 @@
 /**
- * Firmware build version - format: xx.yy.zzzzzz 
+ * Firmware build version - format: xx.yy.zzzzzz
  * x: Major  -  y: Minor  -  z: git short hash generated automaticaly from git
- * i.e.: BUILD_VERSION = 'Rel. v2.08' --> Will generated to: 'v2.08.1a2b3c4' 
+ * i.e.: BUILD_VERSION = 'Rel. v2.08' --> Will generated to: 'v2.08.1a2b3c4'
  */
 
-#define BUILD_VERSION "v2.09"
+#define BUILD_VERSION "v2.10"
