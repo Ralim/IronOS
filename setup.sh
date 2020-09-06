61b3f42e0b3a12b146769b92a94757d119e03672
Ben V. Brown <Ralim@Ralimtek.com>
2020-09-06 13:06:18 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 set -e
 # Setup shell file to setup the environment on an ubuntu machine
-apt-get update && \
-  apt-get install -y \
-  make \
-  bzip2 \
-  git \
-  python3 \
-  wget && \
-  apt-get clean
-mkdir /build
+sudo apt-get update
+sudo apt-get install -y make bzip2 git python3 wget 
+sudo mkdir /build
 cd /build
-wget -qO- https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2 | tar -xj
+
+sudo wget -qO- https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2 | sudo tar -xj
 
 # Add compiler to the path
 export PATH=/build/gcc-arm-none-eabi-9-2020-q2-update/bin:$PATH
