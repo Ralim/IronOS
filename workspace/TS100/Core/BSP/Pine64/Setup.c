65ae7c8b13fffa99e87e46041cf3323f0c673535
Ben V. Brown <Ralim@Ralimtek.com>
2020-09-18 19:09:59 +1000

    Cleanup ADC

--- workspace/TS100/Core/BSP/Pine64/Setup.c
+++ workspace/TS100/Core/BSP/Pine64/Setup.c
@@ -8,8 +8,11 @@
 #include "BSP.h"
 #include "Pins.h"
 #include "gd32vf103.h"
+#include <string.h>
 #include "systick.h"
-uint16_t ADCReadings[64]; // room for 32 lots of the pair of readings
+#define ADC_NORM_CHANNELS 2
+#define ADC_NORM_SAMPLES 32
+uint16_t ADCReadings[ADC_NORM_SAMPLES * ADC_NORM_CHANNELS]; // room for 32 lots of the pair of readings
 
 // Functions
 void setup_gpio();
@@ -42,8 +45,8 @@ void hardware_init() {
 // channel 0 -> temperature sensor, 1-> VIN
 uint16_t getADC(uint8_t channel) {
 	uint32_t sum = 0;
-	for (uint8_t i = 0; i < 32; i++)
-		sum += ADCReadings[channel + (i * 2)];
+	for (uint8_t i = 0; i < ADC_NORM_SAMPLES; i++)
+		sum += ADCReadings[channel + (i * ADC_NORM_CHANNELS)];
 	return sum >> 2;
 }
 
@@ -67,13 +70,12 @@ void setup_gpio() {
 	gpio_init(PWM_Out_GPIO_Port, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ,
 	PWM_Out_Pin);
 	//Analog Inputs ... as analog inputs
-	gpio_init(TMP36_INPUT_GPIO_Port, GPIO_MODE_AIN, GPIO_OSPEED_2MHZ,
+	gpio_init(TMP36_INPUT_GPIO_Port, GPIO_MODE_AIN, GPIO_OSPEED_50MHZ,
 	TMP36_INPUT_Pin);
-	gpio_init(TIP_TEMP_GPIO_Port, GPIO_MODE_AIN, GPIO_OSPEED_2MHZ,
+	gpio_init(TIP_TEMP_GPIO_Port, GPIO_MODE_AIN, GPIO_OSPEED_50MHZ,
 	TIP_TEMP_Pin);
-	gpio_init(VIN_GPIO_Port, GPIO_MODE_AIN, GPIO_OSPEED_2MHZ, VIN_Pin);
-	//Timer 2 remap to move timer 2 ch0 to pin PB4
-//	gpio_pin_remap_config(GPIO_TIMER2_PARTIAL_REMAP, ENABLE);
+	gpio_init(VIN_GPIO_Port, GPIO_MODE_AIN, GPIO_OSPEED_50MHZ, VIN_Pin);
+
 	//Remap PB4 away from JTAG NJRST
 	gpio_pin_remap_config(GPIO_SWJ_NONJTRST_REMAP, ENABLE);
 	//TODO - rest of pins as floating
@@ -83,6 +85,7 @@ void setup_dma() {
 	{
 		/* enable DMA0 clock */
 		rcu_periph_clock_enable(RCU_DMA0);
+		rcu_periph_clock_enable(RCU_DMA1);
 		/* ADC_DMA_channel configuration */
 		dma_parameter_struct dma_data_parameter;
 
@@ -92,12 +95,12 @@ void setup_dma() {
 		/* initialize DMA data mode */
 		dma_data_parameter.periph_addr = (uint32_t) (&ADC_RDATA(ADC0));
 		dma_data_parameter.periph_inc = DMA_PERIPH_INCREASE_DISABLE;
-		dma_data_parameter.memory_addr = (uint32_t) (&ADCReadings);
+		dma_data_parameter.memory_addr = (uint32_t) (ADCReadings);
 		dma_data_parameter.memory_inc = DMA_MEMORY_INCREASE_ENABLE;
-		dma_data_parameter.periph_width = DMA_PERIPHERAL_WIDTH_32BIT;
+		dma_data_parameter.periph_width = DMA_PERIPHERAL_WIDTH_16BIT;
 		dma_data_parameter.memory_width = DMA_MEMORY_WIDTH_16BIT;
 		dma_data_parameter.direction = DMA_PERIPHERAL_TO_MEMORY;
-		dma_data_parameter.number = 64;
+		dma_data_parameter.number = ADC_NORM_SAMPLES * ADC_NORM_CHANNELS;
 		dma_data_parameter.priority = DMA_PRIORITY_HIGH;
 		dma_init(DMA0, DMA_CH0, &dma_data_parameter);
 
@@ -123,88 +126,72 @@ void setup_adc() {
 	//Setup ADC in normal + injected mode
 	//Want it to sample handle temp and input voltage normally via dma
 	//Then injected trigger to sample tip temp
-
-	/* enable ADC0 clock */
+	memset(ADCReadings, 0, sizeof(ADCReadings));
 	rcu_periph_clock_enable(RCU_ADC0);
-	/* enable ADC1 clock */
 	rcu_periph_clock_enable(RCU_ADC1);
-	/* config ADC clock */
-	rcu_adc_clock_config(RCU_CKADC_CKAPB2_DIV8);
-	/* reset ADC */
 	adc_deinit(ADC0);
 	adc_deinit(ADC1);
+	/* config ADC clock */
+	rcu_adc_clock_config(RCU_CKADC_CKAPB2_DIV16);
 	//Run in normal parallel + inserted parallel
 	adc_mode_config(ADC_DAUL_REGULAL_PARALLEL_INSERTED_PARALLEL);
-	adc_special_function_config(ADC0, ADC_CONTINUOUS_MODE | ADC_SCAN_MODE,
-			ENABLE);
-	adc_special_function_config(ADC1, ADC_CONTINUOUS_MODE | ADC_SCAN_MODE,
-			ENABLE);
+	adc_special_function_config(ADC0, ADC_CONTINUOUS_MODE, ENABLE);
+	adc_special_function_config(ADC0, ADC_SCAN_MODE, ENABLE);
+	adc_special_function_config(ADC1, ADC_CONTINUOUS_MODE, ENABLE);
+	adc_special_function_config(ADC1, ADC_SCAN_MODE, ENABLE);
 	//Align right
 	adc_data_alignment_config(ADC0, ADC_DATAALIGN_RIGHT);
 	adc_data_alignment_config(ADC1, ADC_DATAALIGN_RIGHT);
-	//Setup reading 2 channels on regular mode (Handle Temp + )
-	adc_channel_length_config(ADC0, ADC_REGULAR_CHANNEL, 2);
-	adc_channel_length_config(ADC1, ADC_REGULAR_CHANNEL, 2);
+	//Setup reading 2 channels on regular mode (Handle Temp + dc in)
+	adc_channel_length_config(ADC0, ADC_REGULAR_CHANNEL, ADC_NORM_CHANNELS);
+	adc_channel_length_config(ADC1, ADC_REGULAR_CHANNEL, ADC_NORM_CHANNELS);
 	//Setup the two channels
-	adc_regular_channel_config(ADC0, 0, TMP36_ADC0_CHANNEL,
-	ADC_SAMPLETIME_71POINT5); //temp sensor
-	adc_regular_channel_config(ADC1, 0, TMP36_ADC1_CHANNEL,
-	ADC_SAMPLETIME_71POINT5); //temp sensor
-	adc_regular_channel_config(ADC0, 1, VIN_ADC0_CHANNEL,
-	ADC_SAMPLETIME_71POINT5);   //DC Input voltage
-	adc_regular_channel_config(ADC1, 1, VIN_ADC1_CHANNEL,
-	ADC_SAMPLETIME_71POINT5);   //DC Input voltage
+	adc_regular_channel_config(ADC0, 0, TMP36_ADC0_CHANNEL, ADC_SAMPLETIME_71POINT5); //temp sensor
+	adc_regular_channel_config(ADC1, 0, TMP36_ADC1_CHANNEL, ADC_SAMPLETIME_71POINT5); //temp sensor
+	adc_regular_channel_config(ADC0, 1, VIN_ADC0_CHANNEL, ADC_SAMPLETIME_71POINT5);   //DC Input voltage
+	adc_regular_channel_config(ADC1, 1, VIN_ADC1_CHANNEL, ADC_SAMPLETIME_71POINT5);   //DC Input voltage
 	//Setup that we want all 4 inserted readings to be the tip temp
 	adc_channel_length_config(ADC0, ADC_INSERTED_CHANNEL, 4);
 	adc_channel_length_config(ADC1, ADC_INSERTED_CHANNEL, 4);
 	for (int rank = 0; rank < 4; rank++) {
-		adc_inserted_channel_config(ADC0, rank, TIP_TEMP_ADC0_CHANNEL,
-		ADC_SAMPLETIME_1POINT5);
-		adc_inserted_channel_config(ADC1, rank, TIP_TEMP_ADC1_CHANNEL,
-		ADC_SAMPLETIME_1POINT5);
+		adc_inserted_channel_config(ADC0, rank, TIP_TEMP_ADC0_CHANNEL, ADC_SAMPLETIME_1POINT5);
+		adc_inserted_channel_config(ADC1, rank, TIP_TEMP_ADC1_CHANNEL, ADC_SAMPLETIME_1POINT5);
 	}
+	//Setup timer 1 channel 0 to trigger injected measurements
+	adc_external_trigger_source_config(ADC0, ADC_INSERTED_CHANNEL, ADC0_1_EXTTRIG_INSERTED_T1_CH0);
+	adc_external_trigger_source_config(ADC1, ADC_INSERTED_CHANNEL, ADC0_1_EXTTRIG_INSERTED_T1_CH0);
+
+	adc_external_trigger_source_config(ADC0, ADC_REGULAR_CHANNEL, ADC0_1_EXTTRIG_REGULAR_NONE);
+	adc_external_trigger_source_config(ADC1, ADC_REGULAR_CHANNEL, ADC0_1_EXTTRIG_REGULAR_NONE);
 	// Enable triggers for the ADC
 	adc_external_trigger_config(ADC0, ADC_INSERTED_CHANNEL, ENABLE);
 	adc_external_trigger_config(ADC1, ADC_INSERTED_CHANNEL, ENABLE);
-	adc_software_trigger_enable(ADC0, ADC_REGULAR_CHANNEL);
-	adc_software_trigger_enable(ADC1, ADC_REGULAR_CHANNEL);
-	//Setup timer 1 channel 0 to trigger injected measurements
-	adc_external_trigger_source_config(ADC0, ADC_INSERTED_CHANNEL,
-	ADC0_1_EXTTRIG_INSERTED_T1_CH0);
-	adc_external_trigger_source_config(ADC1, ADC_INSERTED_CHANNEL,
-	ADC0_1_EXTTRIG_INSERTED_T1_CH0);
+	adc_external_trigger_config(ADC0, ADC_REGULAR_CHANNEL, ENABLE);
+	adc_external_trigger_config(ADC1, ADC_REGULAR_CHANNEL, ENABLE);
 
-	adc_external_trigger_source_config(ADC0, ADC_REGULAR_CHANNEL,
-	ADC0_1_EXTTRIG_REGULAR_NONE);
-	adc_external_trigger_source_config(ADC1, ADC_REGULAR_CHANNEL,
-	ADC0_1_EXTTRIG_REGULAR_NONE);
-
-	adc_tempsensor_vrefint_disable();
 	adc_watchdog_disable(ADC0);
 	adc_watchdog_disable(ADC1);
 	adc_resolution_config(ADC0, ADC_RESOLUTION_12B);
 	adc_resolution_config(ADC1, ADC_RESOLUTION_12B);
 	/* clear the ADC flag */
-	adc_interrupt_flag_clear(ADC0, ADC_INT_FLAG_EOC);
-	adc_interrupt_flag_clear(ADC0, ADC_INT_FLAG_EOIC);
 	adc_oversample_mode_disable(ADC0);
 	adc_oversample_mode_disable(ADC1);
 	adc_enable(ADC0);
-	adc_enable(ADC1);
 	delay_1ms(1);
 	adc_calibration_enable(ADC0);
+	adc_enable(ADC1);
 	delay_1ms(1);
 	adc_calibration_enable(ADC1);
 	delay_1ms(1);
-	//Enable DMA mode
 	adc_dma_mode_enable(ADC0);
 	//Enable interrupt on end of injected readings
+	adc_interrupt_flag_clear(ADC0, ADC_INT_FLAG_EOC);
+	adc_interrupt_flag_clear(ADC0, ADC_INT_FLAG_EOIC);
 	adc_interrupt_enable(ADC0, ADC_INT_EOIC);
 	eclic_irq_enable(ADC0_1_IRQn, 2, 0);
-	adc_dma_mode_enable(ADC0);
-	adc_dma_mode_enable(ADC1);
-	adc_software_trigger_enable(ADC0, ADC_REGULAR_CHANNEL);
-	adc_software_trigger_enable(ADC1, ADC_REGULAR_CHANNEL);
+//	adc_software_trigger_enable(ADC0, ADC_REGULAR_CHANNEL);
+//	adc_software_trigger_enable(ADC1, ADC_REGULAR_CHANNEL);
+	adc_tempsensor_vrefint_disable();
 }
 void setup_timers() {
 	//Setup timer 1 to run the actual PWM level
