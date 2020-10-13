3e62afc4556777f035de994e73df200fbb708d08
Ben V. Brown <Ralim@Ralimtek.com>
2020-10-13 22:31:46 +1100

    Move to nuclei compiler

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ sudo mkdir /build
 cd /build
 
 sudo wget -qO- https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2 | sudo tar -xj
-sudo wget -qO- https://github.com/riscv-mcu/riscv-gnu-toolchain/releases/download/v9.2RC/rv_linux_bare_1908312208.tar.bz2 | sudo tar -xj
+sudo wget -qO- https://github.com/Ralim/nuclei-compiler/releases/download/2020.08/nuclei_riscv_newlibc_prebuilt_linux64_2020.08.tar.bz2 | sudo tar -xj
 
 # Add compiler to the path
 
 sudo ln -s /build/gcc-arm-none-eabi-9-2020-q2-update/bin/* /usr/local/bin
-sudo ln -s /build/rv_linux_bare_1908312208/bin/* /usr/local/bin
+sudo ln -s /build/gcc/bin/* /usr/local/bin
