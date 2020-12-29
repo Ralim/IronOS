12670461aac21c3d6dfbccdf4e1c8dd12c83b254
Ben V. Brown <Ralim@Ralimtek.com>
2020-12-29 11:38:17 +1100

    Update setup to use cache folder

--- setup.sh
+++ setup.sh
@@ -3,13 +3,31 @@ set -e
 # Setup shell file to setup the environment on an ubuntu machine
 sudo apt-get update 
 sudo apt-get install -y make bzip2 git python3 wget 
-sudo mkdir /build
+sudo mkdir -p /build
 cd /build
 
-sudo wget -qO- https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2 | sudo tar -xj
-sudo wget -qO- https://github.com/Ralim/nuclei-compiler/releases/download/2020.08/nuclei_riscv_newlibc_prebuilt_linux64_2020.08.tar.bz2 | sudo tar -xj
+#Download source files to cache folder
+mkdir -p /build/cache
+cd /build/cache/
+if md5sum -c /build/source/ci/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2.md5; then
+    echo "Good MD5 ARM"
+else
+    echo "ARM MD5 Mismatch, downloading fresh"
+    sudo wget  https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2 -O gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2
+fi
 
-# Add compiler to the path
+if md5sum -c /build/source/ci/nuclei_riscv_newlibc_prebuilt_linux64_2020.08.tar.bz2.md5; then
+    echo "Good MD5 RISCV"
+else
+    echo "RISCV MD5 Mismatch, downloading fresh"
+    sudo wget  https://github.com/Ralim/nuclei-compiler/releases/download/2020.08/nuclei_riscv_newlibc_prebuilt_linux64_2020.08.tar.bz2 -O nuclei_riscv_newlibc_prebuilt_linux64_2020.08.tar.bz2
+fi
+
+echo "Extracting compilers"
+sudo tar -xj -f gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2 -C /build/
+sudo tar -xj -f nuclei_riscv_newlibc_prebuilt_linux64_2020.08.tar.bz2 -C /build/
+
+echo "Link into PATH"
 
 sudo ln -s /build/gcc-arm-none-eabi-9-2020-q2-update/bin/* /usr/local/bin
 sudo ln -s /build/gcc/bin/* /usr/local/bin
