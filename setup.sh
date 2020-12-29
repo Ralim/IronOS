656e80fcf6394f2b6f27b256458affe041f68fa6
Ben V. Brown <Ralim@Ralimtek.com>
2020-12-29 12:00:50 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 set -e
 # Setup shell file to setup the environment on an ubuntu machine
-sudo apt-get update 
-sudo apt-get install -y make bzip2 git python3 wget 
+sudo apt-get update && sudo apt-get install -y make bzip2 git python3 wget 
 sudo mkdir -p /build
 cd /build
 
 #Download source files to cache folder
 # Remember if these are updated, you need to update the md5 file respectively
+# Github checks out under $GITHUB_WORKSPACE
+MDPATH=${GITHUB_WORKSPACE:-/build/source/}
 sudo mkdir -p /build/cache
 cd /build/cache/
-if md5sum -c /build/source/ci/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2.md5; then
+if md5sum -c $MDPATH/ci/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2.md5; then
     echo "Good MD5 ARM"
 else
     echo "ARM MD5 Mismatch, downloading fresh"
     sudo wget -q https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2 -O gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2
 fi
 
-if md5sum -c /build/source/ci/nuclei_riscv_newlibc_prebuilt_linux64_2020.08.tar.bz2.md5; then
+if md5sum -c $MDPATH/ci/nuclei_riscv_newlibc_prebuilt_linux64_2020.08.tar.bz2.md5; then
     echo "Good MD5 RISCV"
 else
     echo "RISCV MD5 Mismatch, downloading fresh"
