513728a1d1e7ee36d1454bd08e55ff7ae8d4aa08
Ryan Jarvis <ryan@technoloft.com>
2021-04-03 17:19:36 -0700

    Cleanup make_translation.py (PEP8, Python3, etc.)

--- Translations/make_translation_test.py
+++ Translations/make_translation_test.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
-# coding=utf-8
 import unittest
 
 
 class TestMakeTranslation(unittest.TestCase):
-    def test_getCharsFromFontIndex(self):
-        from make_translation import getCharsFromFontIndex
+    def test_get_chars_from_font_index(self):
+        from make_translation import get_chars_from_font_index
 
-        self.assertEqual(getCharsFromFontIndex(2), "\\x02")
-        self.assertEqual(getCharsFromFontIndex(239), "\\xEF")
-        self.assertEqual(getCharsFromFontIndex(240), "\\xF0")
-        self.assertEqual(getCharsFromFontIndex(241), "\\xF1\\x01")
-        self.assertEqual(getCharsFromFontIndex(495), "\\xF1\\xFF")
-        self.assertEqual(getCharsFromFontIndex(496), "\\xF2\\x01")
-        self.assertEqual(getCharsFromFontIndex(750), "\\xF2\\xFF")
-        self.assertEqual(getCharsFromFontIndex(751), "\\xF3\\x01")
-        self.assertEqual(getCharsFromFontIndex(0x10 * 0xFF - 15), "\\xFF\\xFF")
-        with self.assertRaises(AssertionError):
-            getCharsFromFontIndex(0x10 * 0xFF - 14)
+        self.assertEqual(get_chars_from_font_index(2), "\\x02")
+        self.assertEqual(get_chars_from_font_index(239), "\\xEF")
+        self.assertEqual(get_chars_from_font_index(240), "\\xF0")
+        self.assertEqual(get_chars_from_font_index(241), "\\xF1\\x01")
+        self.assertEqual(get_chars_from_font_index(495), "\\xF1\\xFF")
+        self.assertEqual(get_chars_from_font_index(496), "\\xF2\\x01")
+        self.assertEqual(get_chars_from_font_index(750), "\\xF2\\xFF")
+        self.assertEqual(get_chars_from_font_index(751), "\\xF3\\x01")
+        self.assertEqual(get_chars_from_font_index(0x10 * 0xFF - 15), "\\xFF\\xFF")
+        with self.assertRaises(ValueError):
+            get_chars_from_font_index(0x10 * 0xFF - 14)
 
 
 if __name__ == "__main__":
