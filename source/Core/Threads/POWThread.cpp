16ac45d74ab51f3b504bbcf6aa834a3336d4870d
Ben V. Brown <Ralim@Ralimtek.com>
2021-04-05 15:04:59 +1000

    Update POWThread.cpp

--- source/Core/Threads/POWThread.cpp
+++ source/Core/Threads/POWThread.cpp
@@ -10,6 +10,7 @@
 #include "QC3.h"
 #include "Settings.h"
 #include "cmsis_os.h"
+#include "fusbpd.h"
 #include "main.hpp"
 #include "stdlib.h"
 #include "task.h"
@@ -17,7 +18,17 @@
 // Small worker thread to handle power (mostly QC) related steps
 
 void startPOWTask(void const *argument __unused) {
+  // You have to run this once we are willing to answer PD messages
+  // Setting up too early can mean that we miss the ~20ms window to respond on some chargers
+#ifdef POW_PD
+  if (usb_pd_detect() == true) {
+    // Spawn all of the USB-C processors
+    fusb302_start_processing();
+  }
+#endif
+  // Init any other misc sensors
   postRToSInit();
+
   for (;;) {
     power_check();
     osDelay(TICKS_100MS); // Slow down update rate
