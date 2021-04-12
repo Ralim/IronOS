11f40cfc1b5b8f935d0ff13f4e72a060f6d2dc3a
Alvin Wong <alvinhochun@gmail.com>
2021-04-12 20:07:06 +0800

    Reformat lzfx sources

--- source/Core/lzfx/lzfx-host-compress.c
+++ source/Core/lzfx/lzfx-host-compress.c
@@ -5,48 +5,44 @@
 
 /* Program to demonstrate file compression. Don't use it in the real world! */
 
-int main(int argc, char **argv)
-{
-	if (argc == 3)
-	{
-		/* open input */
-		FILE *f = fopen(argv[1], "rb");
-		if (!f) {
-			printf("Error: %s\n", argv[1]);
-			return 1;
-		}
+int main(int argc, char **argv) {
+  if (argc == 3) {
+    /* open input */
+    FILE *f = fopen(argv[1], "rb");
+    if (!f) {
+      printf("Error: %s\n", argv[1]);
+      return 1;
+    }
 
-		/* get size */
-		fseek(f, 0, SEEK_END);
-		int inputsize = ftell(f);
-		fseek(f, 0, SEEK_SET);
+    /* get size */
+    fseek(f, 0, SEEK_END);
+    int inputsize = ftell(f);
+    fseek(f, 0, SEEK_SET);
 
-		/* read */
-		char *input = malloc(inputsize);
-		fread(input, inputsize, 1, f);
-		fclose(f);
+    /* read */
+    char *input = malloc(inputsize);
+    fread(input, inputsize, 1, f);
+    fclose(f);
 
-		/* compress */
-		int outputsize = inputsize + 1; /* buffer overflow */
-		char *output = malloc(outputsize);
-		lzfx_compress(input, inputsize, output, &outputsize);
+    /* compress */
+    int   outputsize = inputsize + 1; /* buffer overflow */
+    char *output     = malloc(outputsize);
+    lzfx_compress(input, inputsize, output, &outputsize);
 
-		/* open output */
-		f = fopen(argv[2], "wb");
-		if (!f) {
-			printf("Error: %s\n", argv[1]);
-			return 1;
-		}
+    /* open output */
+    f = fopen(argv[2], "wb");
+    if (!f) {
+      printf("Error: %s\n", argv[1]);
+      return 1;
+    }
 
-		/* write */
-		fwrite(output, outputsize, 1, f);
-		fclose(f);
+    /* write */
+    fwrite(output, outputsize, 1, f);
+    fclose(f);
 
-		return 0;
-	}
-	else
-	{
-		printf("Compresses a file.\n\nUsage: lzfx-raw input output\n");
-		return 1;
-	}
+    return 0;
+  } else {
+    printf("Compresses a file.\n\nUsage: lzfx-raw input output\n");
+    return 1;
+  }
 }
--- source/Core/lzfx/lzfx.h
+++ source/Core/lzfx/lzfx.h
@@ -6,19 +6,19 @@
  * codebase written by Marc Lehmann.  This code is released under the BSD
  * license.  License and original copyright statement follow.
  *
- * 
+ *
  * Copyright (c) 2000-2008 Marc Alexander Lehmann <schmorp@schmorp.de>
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  *   1.  Redistributions of source code must retain the above copyright notice,
  *       this list of conditions and the following disclaimer.
- * 
+ *
  *   2.  Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MER-
  * CHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
@@ -29,7 +29,7 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTH-
  * ERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 
 #ifndef LZFX_H
 #define LZFX_H
@@ -44,19 +44,19 @@ extern "C" {
     to read each other's output, although the output itself is not
     guaranteed to be byte-for-byte identical.
 */
-#define LZFX_VERSION_MAJOR      0
-#define LZFX_VERSION_MINOR      1
-#define LZFX_VERSION_STRING     "0.1"
+#define LZFX_VERSION_MAJOR  0
+#define LZFX_VERSION_MINOR  1
+#define LZFX_VERSION_STRING "0.1"
 
 /* Hashtable size (2**LZFX_HLOG entries) */
 #ifndef LZFX_HLOG
-# define LZFX_HLOG 16
+#define LZFX_HLOG 16
 #endif
 
 /* Predefined errors. */
-#define LZFX_ESIZE      -1      /* Output buffer too small */
-#define LZFX_ECORRUPT   -2      /* Invalid data for decompression */
-#define LZFX_EARGS      -3      /* Arguments invalid (NULL) */
+#define LZFX_ESIZE    -1 /* Output buffer too small */
+#define LZFX_ECORRUPT -2 /* Invalid data for decompression */
+#define LZFX_EARGS    -3 /* Arguments invalid (NULL) */
 
 /*  Buffer-to buffer compression.
 
@@ -67,8 +67,7 @@ extern "C" {
     olen contains the compressed size in bytes.  On failure, a negative
     value is returned and olen is not modified.
 */
-int lzfx_compress(const void* ibuf, unsigned int ilen,
-                        void* obuf, unsigned int *olen);
+int lzfx_compress(const void *ibuf, unsigned int ilen, void *obuf, unsigned int *olen);
 
 /*  Buffer-to-buffer decompression.
 
@@ -87,9 +86,7 @@ int lzfx_compress(const void* ibuf, unsigned int ilen,
     stream and is consequently very fast.  Argument obuf may be NULL in
     this case only.
 */
-int lzfx_decompress(const void* ibuf, unsigned int ilen,
-                          void* obuf, unsigned int *olen);
-
+int lzfx_decompress(const void *ibuf, unsigned int ilen, void *obuf, unsigned int *olen);
 
 #ifdef __cplusplus
 } /* extern "C" */
