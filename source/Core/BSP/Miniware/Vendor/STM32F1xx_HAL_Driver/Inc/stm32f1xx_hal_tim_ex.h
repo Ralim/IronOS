c4512210a6a113714c9e32b01357cc22e5e33674
Ben V. Brown <Ralim@Ralimtek.com>
2021-05-21 20:42:13 +1000

    Update stm32f1xx_hal_tim_ex.h

--- source/Core/BSP/Miniware/Vendor/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
+++ source/Core/BSP/Miniware/Vendor/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
@@ -136,24 +136,6 @@ typedef struct {
        /* defined(STM32F103x6) || defined(STM32F103xB) || defined(STM32F103xE) || defined(STM32F103xG) || */
        /* defined(STM32F105xC) || defined(STM32F107xC)                                                    */
 
-/* Exported macro ------------------------------------------------------------*/
-/**
- * @brief  Resets the TIM Output compare preload.
- * @param  __HANDLE__: TIM handle.
- * @param  __CHANNEL__: TIM Channels to be configured.
- *          This parameter can be one of the following values:
- *            @arg TIM_CHANNEL_1: TIM Channel 1 selected
- *            @arg TIM_CHANNEL_2: TIM Channel 2 selected
- *            @arg TIM_CHANNEL_3: TIM Channel 3 selected
- *            @arg TIM_CHANNEL_4: TIM Channel 4 selected
- * @retval None
- */
-#define __HAL_TIM_DISABLE_OCxPRELOAD(__HANDLE__, __CHANNEL__)                                                                                                             \
-  (((__CHANNEL__) == TIM_CHANNEL_1) ? ((__HANDLE__)->Instance->CCMR1 &= (uint16_t)~TIM_CCMR1_OC1PE)                                                                       \
-                                    : ((__CHANNEL__) == TIM_CHANNEL_2) ? ((__HANDLE__)->Instance->CCMR1 &= (uint16_t)~TIM_CCMR1_OC2PE)                                    \
-                                                                       : ((__CHANNEL__) == TIM_CHANNEL_3) ? ((__HANDLE__)->Instance->CCMR2 &= (uint16_t)~TIM_CCMR2_OC3PE) \
-                                                                                                          : ((__HANDLE__)->Instance->CCMR2 &= (uint16_t)~TIM_CCMR2_OC4PE))
-
 /* Exported functions --------------------------------------------------------*/
 /** @addtogroup TIMEx_Exported_Functions
  * @{
