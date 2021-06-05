9ed1c1fc043736af873eb8164af1b62dfc7cb4c5
Ben V. Brown <Ralim@Ralimtek.com>
2021-06-06 08:35:40 +1000

    Update cache invalidator

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ python3 -m pip install bdflib black flake8
 sudo mkdir -p /build
 cd /build
 
-#Download source files to cache folder
+# Download source files to cache folder
 # Remember if these are updated, you need to update the md5 file respectively
 # Github checks out under $GITHUB_WORKSPACE
 MDPATH=${GITHUB_WORKSPACE:-/build/source/}
@@ -16,16 +16,16 @@ if md5sum -c $MDPATH/ci/gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2.
     echo "Good MD5 ARM"
 else
     echo "ARM MD5 Mismatch, downloading fresh"
+    rm -rf /build/cache/gcc-arm*.bz2 || true
     sudo wget -q "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2" -O gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2
-    rm -rf $MDPATH/ci/gcc-arm*.bz2 || true
 fi
 
 if md5sum -c $MDPATH/ci/nuclei_riscv_newlibc_prebuilt_linux64_2020.08.tar.bz2.md5; then
     echo "Good MD5 RISCV"
 else
     echo "RISCV MD5 Mismatch, downloading fresh"
+    rm -rf /build/cache/nuclei*.bz2 || true
     sudo wget -q "https://github.com/Ralim/nuclei-compiler/releases/download/2020.08/nuclei_riscv_newlibc_prebuilt_linux64_2020.08.tar.bz2" -O nuclei_riscv_newlibc_prebuilt_linux64_2020.08.tar.bz2
-    rm -rf $MDPATH/ci/nuclei*.bz2 || true
 fi
 
 echo "Extracting compilers"
