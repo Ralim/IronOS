30c43c5f425763ba00addf8e9013c83c0ade1e7f
discip <53649486+discip@users.noreply.github.com>
2021-09-27 02:01:02 +0200

    Update Settings.cpp

--- source/Core/Src/Settings.cpp
+++ source/Core/Src/Settings.cpp
@@ -82,7 +82,7 @@ static const SettingConstants settingsConstants[(int)SettingsOptions::SettingsOp
     {0, 0xFFFF, 0, 41431 /*EN*/},                                   // UILanguage
     {0, 51, 1, 0},                                                  // PDNegTimeout
     {0, 2, 1, 0},                                                   // OLEDInversion
-    {5, 256, 25, 55},                                               // OLEDBrightness
+    {3, 256, 28, 59},                                               // OLEDBrightness
 
 };
 static_assert((sizeof(settingsConstants) / sizeof(SettingConstants)) == ((int)SettingsOptions::SettingsOptionsLength));
