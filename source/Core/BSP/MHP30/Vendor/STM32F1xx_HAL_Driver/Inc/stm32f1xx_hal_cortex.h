62470dcfb7ecfd611a3314f851b37a9fad936338
Ben V. Brown <ralim@ralimtek.com>
2021-10-01 22:27:18 +1000

    MHP format

--- source/Core/BSP/MHP30/Vendor/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+++ source/Core/BSP/MHP30/Vendor/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
@@ -1,21 +1,21 @@
 /**
- ******************************************************************************
- * @file    stm32f1xx_hal_cortex.h
- * @author  MCD Application Team
- * @brief   Header file of CORTEX HAL module.
- ******************************************************************************
- * @attention
- *
- * <h2><center>&copy; Copyright (c) 2017 STMicroelectronics.
- * All rights reserved.</center></h2>
- *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
- *
- ******************************************************************************
- */
+******************************************************************************
+* @file    stm32f1xx_hal_cortex.h
+* @author  MCD Application Team
+* @brief   Header file of CORTEX HAL module.
+******************************************************************************
+* @attention
+*
+* <h2><center>&copy; Copyright (c) 2017 STMicroelectronics.
+* All rights reserved.</center></h2>
+*
+* This software component is licensed by ST under BSD 3-Clause license,
+* the "License"; You may not use this file except in compliance with the
+* License. You may obtain a copy of the License at:
+*                        opensource.org/licenses/BSD-3-Clause
+*
+******************************************************************************
+*/
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32F1xx_HAL_CORTEX_H
--- source/Core/BSP/MHP30/Vendor/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+++ source/Core/BSP/MHP30/Vendor/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
@@ -1,22 +1,22 @@
 /**
- ******************************************************************************
- * @file    stm32f1xx_hal_def.h
- * @author  MCD Application Team
- * @brief   This file contains HAL common defines, enumeration, macros and
- *          structures definitions.
- ******************************************************************************
- * @attention
- *
- * <h2><center>&copy; Copyright (c) 2017 STMicroelectronics.
- * All rights reserved.</center></h2>
- *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
- *
- ******************************************************************************
- */
+******************************************************************************
+* @file    stm32f1xx_hal_def.h
+* @author  MCD Application Team
+* @brief   This file contains HAL common defines, enumeration, macros and
+*          structures definitions.
+******************************************************************************
+* @attention
+*
+* <h2><center>&copy; Copyright (c) 2017 STMicroelectronics.
+* All rights reserved.</center></h2>
+*
+* This software component is licensed by ST under BSD 3-Clause license,
+* the "License"; You may not use this file except in compliance with the
+* License. You may obtain a copy of the License at:
+*                        opensource.org/licenses/BSD-3-Clause
+*
+******************************************************************************
+*/
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32F1xx_HAL_DEF
