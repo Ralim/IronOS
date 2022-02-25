cf514b8a596fc5f074e4c70365f6bca178cf440e
discip <53649486+discip@users.noreply.github.com>
2022-02-25 23:02:05 +0100

    Update FreeRTOSConfig.h

--- source/Core/BSP/Miniware/FreeRTOSConfig.h
+++ source/Core/BSP/Miniware/FreeRTOSConfig.h
@@ -123,7 +123,7 @@ extern uint32_t SystemCoreClock;
 #define INCLUDE_vTaskDelete                 0
 #define INCLUDE_vTaskCleanUpResources       0
 #define INCLUDE_vTaskSuspend                0
-#define INCLUDE_vTaskDelayUntil             0
+#define INCLUDE_vTaskDelayUntil             1
 #define INCLUDE_vTaskDelay                  1
 #define INCLUDE_xTaskGetSchedulerState      1
 #define INCLUDE_uxTaskGetStackHighWaterMark 1
