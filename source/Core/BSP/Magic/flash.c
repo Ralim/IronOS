49be70c667ce58bee67fcf2818d539f00e63aa7a
Ben V. Brown <Ralim@Ralimtek.com>
2022-04-22 18:22:33 +1000

    Draft flash settings

--- source/Core/BSP/Magic/flash.c
+++ source/Core/BSP/Magic/flash.c
@@ -7,17 +7,25 @@
 
 #include "BSP.h"
 #include "BSP_Flash.h"
+#include "hal_flash.h"
 #include "string.h"
-#define FMC_PAGE_SIZE ((uint16_t)0x400U)
-// static uint16_t settings_page[FMC_PAGE_SIZE] __attribute__ ((section (".settings_page")));
-// Linker script doesnt want to play, so for now its hard coded
-#define SETTINGS_START_PAGE (0x08000000 + (127 * 1024))
+
+#define FLASH_PAGE_SIZE ((uint16_t)1024)
+
+#define SETTINGS_START_PAGE (1023 * 1024) // Hal auto offsets base addr
+#define SETTINGS_READ_PAGE  0x23000000 + SETTINGS_START_PAGE
 uint8_t flash_save_buffer(const uint8_t *buffer, const uint16_t length) {
-  // #TODO
+  flash_erase(SETTINGS_START_PAGE, length);
+
+  flash_write(SETTINGS_START_PAGE, buffer, sizeof(length));
+
   return 1;
 }
 
 void flash_read_buffer(uint8_t *buffer, const uint16_t length) {
+
+  /* read 0x00010000 flash data */
+  flash_read(SETTINGS_START_PAGE, buffer, sizeof(length));
   return;
   // #TODO
   uint32_t *b  = (uint32_t *)buffer;
