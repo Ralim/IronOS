e233b3ec14b7d36f9eafeefa7353030f93023b27
Ben V. Brown <Ralim@Ralimtek.com>
2022-06-16 20:18:52 +1000

    Update PIDThread.cpp

--- source/Core/Threads/PIDThread.cpp
+++ source/Core/Threads/PIDThread.cpp
@@ -76,10 +76,10 @@ void startPIDTask(void const *argument __unused) {
       }
       setOutputx10WattsViaFilters(x10WattsOut);
       // If the output is off, take tip measurement reading
-      if (x10WattsOut == 0 && PIDTempTarget == 0) {
-        startMeasureTipResistance();
-        measuringTipResistance = true;
-      }
+      // if (x10WattsOut == 0 && PIDTempTarget == 0) {
+      //   startMeasureTipResistance();
+      //   measuringTipResistance = true;
+      // }
     } else {
       // ADC interrupt timeout
       setTipPWM(0, false);
