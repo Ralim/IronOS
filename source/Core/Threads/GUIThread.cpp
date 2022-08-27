6b7e304c319f098f70ee820a5ed6bf3f4d621183
discip <53649486+discip@users.noreply.github.com>
2022-08-28 01:17:59 +0200

    Update GUIThread.cpp

--- source/Core/Threads/GUIThread.cpp
+++ source/Core/Threads/GUIThread.cpp
@@ -997,7 +997,7 @@ void startGUITask(void const *argument) {
 #endif
 #endif
   // al_qu Calibrate Cold Junction Compensation directly at boot, before internal components get warm.
-  if (tipDisconnected == false && tipTemp <= 32) {
+  if (istipDisconnected == false) { // && tipTemp <= 32
     uint16_t setoffset = 0;
     // If the thermo-couple at the end of the tip, and the handle are at
     // equilibrium, then the output should be zero, as there is no temperature
