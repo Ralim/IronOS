9673ffd7535c6fae0f11af4f5ee9104629ad8a2a
discip <53649486+discip@users.noreply.github.com>
2022-08-28 14:03:33 +0200

    Update GUIThread.cpp

--- source/Core/Threads/GUIThread.cpp
+++ source/Core/Threads/GUIThread.cpp
@@ -1001,7 +1001,7 @@ void startGUITask(void const *argument) {
 #endif
 #endif
   // al_qu Calibrate Cold Junction Compensation directly at boot, before internal components get warm.
-  uint32_t tipTemp = TipThermoModel::getTipInC();
+  // uint32_t tipTemp = TipThermoModel::getTipInC();
   if (!isTipDisconnected() && tipTemp <= 30) {
     uint16_t setoffset = 0;
     // If the thermo-couple at the end of the tip, and the handle are at
