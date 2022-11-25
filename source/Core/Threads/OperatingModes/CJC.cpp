cc65cbb05e92f70843de636dff69cb6cb40bbafc
Ben V. Brown <Ralim@Ralimtek.com>
2022-11-25 21:25:49 +1100

    Update CJC.cpp

--- source/Core/Threads/OperatingModes/CJC.cpp
+++ source/Core/Threads/OperatingModes/CJC.cpp
@@ -5,7 +5,7 @@ void performCJCC(void) {
   // Calibrate Cold Junction Compensation directly at boot, before internal components get warm.
   OLED::refresh();
   osDelay(50);
-  if (!isTipDisconnected() && (TipThermoModel::getTipInC() - getHandleTemperature(0) / 10) < 10) {
+  if (!isTipDisconnected() && abs(int(TipThermoModel::getTipInC() - getHandleTemperature(0) / 10)) < 10) {
     uint16_t setoffset = 0;
     // If the thermo-couple at the end of the tip, and the handle are at
     // equilibrium, then the output should be zero, as there is no temperature
