c94d128e0410504b41ca0778787ccd7e5fd14de5
discip <53649486+discip@users.noreply.github.com>
2023-03-13 17:53:46 +0100

    Update configuration.h

--- source/Core/BSP/Pinecil/configuration.h
+++ source/Core/BSP/Pinecil/configuration.h
@@ -63,8 +63,8 @@
  * OLED Brightness
  *
  */
-#define MIN_BRIGHTNESS             0   // Min OLED brightness selectable
-#define MAX_BRIGHTNESS             100 // Max OLED brightness selectable
+#define MIN_BRIGHTNESS             1   // Min OLED brightness selectable
+#define MAX_BRIGHTNESS             101 // Max OLED brightness selectable
 
 /**
  * Temp change settings
