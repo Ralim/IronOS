3a05d695f86522e8ce57edd66117aced29e868a9
Ben V. Brown <Ralim@Ralimtek.com>
2023-06-29 18:46:26 +1000

    More Font utils

--- source/Core/Inc/FontUtils.h
+++ source/Core/Inc/FontUtils.h
@@ -8,5 +8,6 @@ enum class FontStyle {
   FROM_HEIGHT, // Pick font to best fill the height
 };
 
-constexpr uint8_t get_fontstyle_height(const FontStyle font);
-constexpr uint8_t get_fontstyle_width(const FontStyle font);
+constexpr uint8_t   get_fontstyle_height(const FontStyle font);
+constexpr uint8_t   get_fontstyle_width(const FontStyle font);
+constexpr FontStyle get_fontstyle_fromHeight(const uint8_t height);
--- source/Core/Src/FontUtils.cpp
+++ source/Core/Src/FontUtils.cpp
@@ -10,3 +10,5 @@ constexpr uint8_t get_fontstyle_width(const FontStyle font) {
     return 6;
   return 12;
 }
+
+constexpr FontStyle get_fontstyle_fromHeight(const uint8_t height) { return (height <= 8) ? FontStyle::SMALL : FontStyle::LARGE; }
