dafc49f5d3e6c48884a425f2d4b47dba1f3be046
Ivan Zorin <ivan.a.zorin@gmail.com>
2023-07-05 11:41:01 +0300

    version.h: update BUILD_VERSION policy / PoC

--- source/version.h
+++ source/version.h
@@ -1,7 +1,13 @@
 /**
- * Firmware build version - format: xx.yy.zzzzzz
+ * Firmware build version - format: xx.yy.zzzzzzzz
  * x: Major  -  y: Minor  -  z: git short hash generated automaticaly from git
- * i.e.: BUILD_VERSION = 'Rel. v2.08' --> Will generated to: 'v2.08.1a2b3c4'
+ * i.e.: BUILD_VERSION = 'v2.08' --> Will generated to: 'v2.08.1a2b3c4d'
+ *
+ * vX.NM      - means stable release version with a related git release tag
+ * vX.N(M+1)d - means a version under development with next rel.ver. X.N(M+1)
+ * i.e.:
+ *   BUILD_VERSION = 'v2.21'  --> Stable release
+ *   BUILD_VERSION = 'v2.22d' --> Devel. for the next stable release of 2.22
  */
 
-#define BUILD_VERSION "v2.21"
+#define BUILD_VERSION "v2.22d"
