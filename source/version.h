f8315175e1840fa86ec4551c649b72a8fd2821a2
Ivan Zorin <ivan.a.zorin@gmail.com>
2023-07-05 11:56:00 +0300

    Fix misplaced chars

--- source/version.h
+++ source/version.h
@@ -3,8 +3,8 @@
  * x: Major  -  y: Minor  -  z: git short hash generated automaticaly from git
  * i.e.: BUILD_VERSION = 'v2.08' --> Will generated to: 'v2.08.1a2b3c4d'
  *
- * vX.NM      - means stable release version with a related git release tag
- * vX.N(M+1)d - means a version under development with next rel.ver. X.N(M+1)
+ * vX.MN      - means stable release version with a related git release tag
+ * vX.M(N+1)d - means a version under development with next rel.ver. X.M(N+1)
  * i.e.:
  *   BUILD_VERSION = 'v2.21'  --> Stable release
  *   BUILD_VERSION = 'v2.22d' --> Devel. for the next stable release of 2.22
