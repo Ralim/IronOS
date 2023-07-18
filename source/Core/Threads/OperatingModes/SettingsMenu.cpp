f5246189f4ff290f9bb6d9053801728156c76f0e
Ben V. Brown <Ralim@Ralimtek.com>
2023-07-18 20:38:36 +1000

    Update SettingsMenu.cpp

--- source/Core/Threads/OperatingModes/SettingsMenu.cpp
+++ source/Core/Threads/OperatingModes/SettingsMenu.cpp
@@ -1,4 +1,39 @@
 #include "OperatingModes.h"
+
+OperatingMode handleSettingsButtons(const ButtonState buttons, guiContext *cxt) {
+  switch (buttons) {
+  case BUTTON_NONE:
+    // Do nothing
+    break;
+  case BUTTON_BOTH:
+    break;
+
+  case BUTTON_B_LONG:
+    return OperatingMode::DebugMenuReadout;
+    break;
+  case BUTTON_F_LONG:
+#ifdef PROFILE_SUPPORT
+    if (!isTipDisconnected()) {
+      return OperatingMode::SolderingProfile;
+    }
+#else
+    return OperatingMode::TemperatureAdjust;
+#endif
+    break;
+  case BUTTON_F_SHORT:
+    if (!isTipDisconnected()) {
+      return OperatingMode::Soldering;
+    }
+    break;
+  case BUTTON_B_SHORT:
+    return OperatingMode::SettingsMenu;
+    break;
+  default:
+    break;
+  }
+  return OperatingMode::HomeScreen;
+}
+
 OperatingMode gui_SettingsMenu(const ButtonState buttons, guiContext *cxt) {
   // Render out the current settings menu
   // State 1 -> Root menu
@@ -6,5 +41,5 @@ OperatingMode gui_SettingsMenu(const ButtonState buttons, guiContext *cxt) {
   uint16_t *mainEntry = &(cxt->scratch_state.state1);
   uint16_t *subEntry  = &(cxt->scratch_state.state2);
 
-  return OperatingMode::HomeScreen;
+  return handleSettingsButtons(buttons, cxt);
 }
