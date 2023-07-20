121b1f46189fbed6f5bc26f7b2788376a98cbf39
Ivan Zorin <ivan.a.zorin@gmail.com>
2023-07-20 04:44:01 +0300

    scripts/ci/buildAll.sh: fix shellcheck and add additional comment

--- scripts/ci/buildAll.sh
+++ scripts/ci/buildAll.sh
@@ -2,11 +2,13 @@
 set -e
 set -u
 
-# Init vars & prepare dir
+# Init vars
 dir_ci="/build/ci"
-dir_artefacts="${dir_ci}/artefacts"
 dir_ironos="/build/ironos"
 dir_source="${dir_ironos}/source"
+
+# Prepare output dir
+dir_artefacts="${dir_ci}/artefacts"
 mkdir -p "${dir_artefacts}"
 
 # Build firmware
@@ -15,4 +17,5 @@ bash ./build.sh || exit 1
 echo "All Firmware built"
 
 # Copy out all the final resulting files we would like to store for the next op
-cp -r "${dir_source}"/Hexfile/{*.hex,*.bin}  "${dir_artefacts}"
+cp -r "${dir_source}"/Hexfile/*.bin  "${dir_artefacts}"
+cp -r "${dir_source}"/Hexfile/*.hex  "${dir_artefacts}"
