385f816f1cb4d048c8b40ba7ea70ebdc10616d8d
Ben V. Brown <Ralim@Ralimtek.com>
2023-07-20 20:31:06 +1000

    Fixup

--- source/Core/Threads/OperatingModes/utils/SolderingCommon.h
+++ source/Core/Threads/OperatingModes/utils/SolderingCommon.h
@@ -1,8 +1,10 @@
 #ifndef SOLDERING_COMMON_H
 #define SOLDERING_COMMON_H
+#include <stdint.h>
 
-void detailedPowerStatus();
-void basicSolderingStatus(bool boostModeOn);
-bool checkExitSoldering();
+void     detailedPowerStatus();
+void     basicSolderingStatus(bool boostModeOn);
+bool     checkExitSoldering();
+uint16_t getTipTemp(void);
 
-#endif //SOLDERING_COMMON_H
+#endif // SOLDERING_COMMON_H
