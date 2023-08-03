0d1c8ab29f4b90f52f7cc5db532cfc8cab917213
Ivan Zorin <ivan.a.zorin@gmail.com>
2023-08-03 12:31:57 +0300

    Testing upper()

--- Translations/make_translation.py
+++ Translations/make_translation.py
@@ -1265,15 +1265,15 @@ def get_translation_sanity_checks_text(defs: dict) -> str:
 
 def get_version_suffix(ver) -> str:
     # Check env var from push.yml first:
-    # if it's pull request then use vX.YY + C.ID for version line as in *C*I with proper tag instead of merge tag for detached tree
+    # - if it's pull request then use vX.YY + C.ID for version line as in *C*I with proper tag instead of merge tag for detached tree
     try:
         print("====>>>> GITHUB_CI_PR_SHA:", os.environ["GITHUB_CI_PR_SHA"])
         if os.environ["GITHUB_CI_PR_SHA"] != "":
-            return "C" + "." + os.environ["GITHUB_CI_PR_SHA"][:8]
+            return "C" + "." + os.environ["GITHUB_CI_PR_SHA"][:8].upper()
     except KeyError:
         print("====>>>> GITHUB_CI_PR_SHA:", "FALSE")
         True
-    # No github PR SHA ID, hence "normal" build
+    # - no github PR SHA ID, hence "normal" build
 
     suffix = str("")
 
