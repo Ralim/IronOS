31b35e1401ecd880d400fd9a8d25b2bc247bc0bb
Ben V. Brown <Ralim@Ralimtek.com>
2023-11-15 18:49:40 +1100

    MHP30 move to I2C Bit Banging

--- source/Core/BSP/MHP30/Software_I2C.h
+++ source/Core/BSP/MHP30/Software_I2C.h
@@ -18,13 +18,26 @@
 #define SOFT_SDA2_LOW()  HAL_GPIO_WritePin(SDA2_GPIO_Port, SDA2_Pin, GPIO_PIN_RESET)
 #define SOFT_SDA2_READ() (HAL_GPIO_ReadPin(SDA2_GPIO_Port, SDA2_Pin) == GPIO_PIN_SET ? 1 : 0)
 #define SOFT_SCL2_READ() (HAL_GPIO_ReadPin(SCL2_GPIO_Port, SCL2_Pin) == GPIO_PIN_SET ? 1 : 0)
+
+#endif
+
+#ifdef I2C_SOFT_BUS_1
+#define SOFT_SCL1_HIGH() HAL_GPIO_WritePin(SCL_GPIO_Port, SCL_Pin, GPIO_PIN_SET)
+#define SOFT_SCL1_LOW()  HAL_GPIO_WritePin(SCL_GPIO_Port, SCL_Pin, GPIO_PIN_RESET)
+#define SOFT_SDA1_HIGH() HAL_GPIO_WritePin(SDA_GPIO_Port, SDA_Pin, GPIO_PIN_SET)
+#define SOFT_SDA1_LOW()  HAL_GPIO_WritePin(SDA_GPIO_Port, SDA_Pin, GPIO_PIN_RESET)
+#define SOFT_SDA1_READ() (HAL_GPIO_ReadPin(SDA_GPIO_Port, SDA_Pin) == GPIO_PIN_SET ? 1 : 0)
+#define SOFT_SCL1_READ() (HAL_GPIO_ReadPin(SCL_GPIO_Port, SCL_Pin) == GPIO_PIN_SET ? 1 : 0)
+
+#endif
+
 #define SOFT_I2C_DELAY()              \
   {                                   \
-    for (int xx = 0; xx < 20; xx++) { \
+    for (int xx = 0; xx < 15; xx++) { \
       asm("nop");                     \
     }                                 \
   }
 
-#endif
+// 40 ~= 100kHz; 15 gives around 250kHz or so which is fast _and_ stable
 
 #endif /* BSP_MINIWARE_SOFTWARE_I2C_H_ */
