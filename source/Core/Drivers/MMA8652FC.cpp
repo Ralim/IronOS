3f797d335f3ad302961582bdc51079f3c712c5df
Ben V. Brown <ralim@ralimtek.com>
2023-11-15 09:54:22 +0000

    format
    format

--- source/Core/Drivers/MMA8652FC.cpp
+++ source/Core/Drivers/MMA8652FC.cpp
@@ -10,7 +10,6 @@
 #include "cmsis_os.h"
 #include <array>
 
-
 static const ACCEL_I2C_CLASS::I2C_REG i2c_registers[] = {
     {CTRL_REG2, 0, 0},                // Normal mode
     {CTRL_REG2, 0x40, 2},             // Reset all registers to POR values
