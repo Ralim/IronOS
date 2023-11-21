faf2c6733ed4d48429f05a97ee5fa37c1de15364
discip <53649486+discip@users.noreply.github.com>
2023-11-22 00:07:10 +0100

    Update settingsGUI.cpp

--- source/Core/Src/settingsGUI.cpp
+++ source/Core/Src/settingsGUI.cpp
@@ -842,10 +842,10 @@ static void displayLogoTime(void) {
     OLED::print(translatedString(Tr->OffString), FontStyle::LARGE);
     break;
   case logoMode_t::ONETIME:
-    OLED::drawArea(OLED_WIDTH - 24 - 2, 0, 24, 16, infinityOnce);
+    OLED::drawArea(OLED_WIDTH - 16 - 2, 0, 16, 16, infinityOnce);
     break;
   case logoMode_t::INFINITY:
-    OLED::drawArea(OLED_WIDTH - 24 - 2, 0, 24, 16, infinityLoop);
+    OLED::drawArea(OLED_WIDTH - 16 - 2, 0, 16, 16, infinityLoop);
     break;
   default:
     OLED::printNumber(getSettingValue(SettingsOptions::LOGOTime), 2, FontStyle::LARGE);
