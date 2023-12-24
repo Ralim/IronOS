ed521189271249bf1f1ddc99ba713839ca7f4ef9
Ben V. Brown <Ralim@Ralimtek.com>
2023-12-24 18:55:47 +1100

    Warning timeout

--- source/Core/Threads/OperatingModes/utils/ShowWarning.cpp
+++ source/Core/Threads/OperatingModes/utils/ShowWarning.cpp
@@ -4,6 +4,9 @@
 bool warnUser(const char *warning, const ButtonState buttons) {
   OLED::clearScreen();
   OLED::printWholeScreen(warning);
-  // TODO also timeout
+  // Also timeout after 5 seconds
+  if ((xTaskGetTickCount() - lastButtonTime) > TICKS_SECOND * 5) {
+    return true;
+  }
   return buttons != BUTTON_NONE;
 }
