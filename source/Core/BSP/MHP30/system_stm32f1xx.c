1c6cd5a22c7d2e35345288c63e9936b1e7001c05
Ben V. Brown <Ralim@Ralimtek.com>
2023-12-24 11:23:05 +1100

    Style updates

--- source/Core/BSP/MHP30/system_stm32f1xx.c
+++ source/Core/BSP/MHP30/system_stm32f1xx.c
@@ -83,7 +83,7 @@ void SystemInit(void) {
 #ifdef VECT_TAB_SRAM
   SCB->VTOR = SRAM_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal SRAM. */
 #else
-  SCB->VTOR  = FLASH_BASE | 0x8000; /* Vector Table Relocation in Internal FLASH. */
+  SCB->VTOR = FLASH_BASE | 0x8000; /* Vector Table Relocation in Internal FLASH. */
 #endif
 }
 
