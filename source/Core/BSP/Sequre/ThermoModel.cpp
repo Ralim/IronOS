78755f9e53d26a330f0c930a779dc84eea4d542e
Ben V. Brown <Ralim@Ralimtek.com>
2024-06-08 17:41:04 +1000

    Update ThermoModel.cpp

--- source/Core/BSP/Sequre/ThermoModel.cpp
+++ source/Core/BSP/Sequre/ThermoModel.cpp
@@ -8,4 +8,9 @@
 #include "Utils.h"
 #include "configuration.h"
 
+#ifdef TEMP_uV_LOOKUP_PT100_1K_PULLUP
+
+#endif // TEMP_uV_LOOKUP_PT100_1K_PULLUP
+#ifdef TEMP_uV_LOOKUP_S60
 TemperatureType_t TipThermoModel::convertuVToDegC(uint32_t tipuVDelta) { return (tipuVDelta * 50) / 485; }
+#endif // TEMP_uV_LOOKUP_S60
