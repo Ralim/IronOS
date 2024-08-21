21cc0509635f812d27c1bd1ff5a3b8117407fea4
ave <ave@ave.zone>
2024-08-21 06:06:32 +0200

    fix clang-format issue in getHallEffectSleepTimeout

--- source/Core/Threads/UI/logic/utils/getHallEffectSleepTimeout.cpp
+++ source/Core/Threads/UI/logic/utils/getHallEffectSleepTimeout.cpp
@@ -3,12 +3,9 @@
 #ifndef NO_SLEEP_MODE
 #ifdef HALL_SENSOR
 uint32_t getHallEffectSleepTimeout(void) {
-
   if (getSettingValue(SettingsOptions::HallEffectSensitivity) && getSettingValue(SettingsOptions::HallEffectSleepTime)) {
-
     uint32_t sleepThres = getSettingValue(SettingsOptions::HallEffectSleepTime) * 5 * TICKS_SECOND;
     return sleepThres;
-
   }
   return TICKS_SECOND;
 }
