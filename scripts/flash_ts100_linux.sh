5c9993ed7ee40203f94aded0bfb1921e335e9a1e
MyNameIsKitsune <mynameiskitsune@proton.me>
2024-11-16 11:31:33 +0200

    Using dd instead cp and unmount tmp folder immediately

--- scripts/flash_ts100_linux.sh
+++ scripts/flash_ts100_linux.sh
@@ -126,14 +126,13 @@ echo "Found TS100 config disk device on $DEVICE"
 
 mount_ts100
 echo "Mounted config disk drive, flashing..."
-cp -v "$1" "$HEX_FIRMWARE"
-sync
+dd if="$1" of="$HEX_FIRMWARE" oflag=direct
+umount_ts100
 
 echo "Waiting for TS100 to flash"
 sleep 5
 
 echo "Remounting config disk drive"
-umount_ts100
 wait_for_ts100
 mount_ts100
 check_flash
