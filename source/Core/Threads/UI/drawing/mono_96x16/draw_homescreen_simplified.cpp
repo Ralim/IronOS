71551e6db516f2c6da1597e49a5b0590cdf883e2
Leo <resistancelion@gmail.com>
2025-01-28 14:32:34 +0200

    Update draw_homescreen_simplified.cpp
    Rework of 96x12 drawer, in order to support swapping of setting/heating hints

--- source/Core/Threads/UI/drawing/mono_96x16/draw_homescreen_simplified.cpp
+++ source/Core/Threads/UI/drawing/mono_96x16/draw_homescreen_simplified.cpp
@@ -6,57 +6,38 @@ extern uint8_t buttonBF[sizeof(buttonB)];
 extern uint8_t disconnectedTipF[sizeof(disconnectedTip)];
 
 void ui_draw_homescreen_simplified(TemperatureType_t tipTemp) {
-  bool tempOnDisplay          = false;
-  bool tipDisconnectedDisplay = false;
-  if (OLED::getRotation()) {
-    OLED::drawArea(54, 0, 42, 16, buttonAF);
-    OLED::drawArea(12, 0, 42, 16, buttonBF);
-    OLED::setCursor(0, 0);
-    ui_draw_power_source_icon();
-  } else {
-    OLED::drawArea(0, 0, 42, 16, buttonA);  // Needs to be flipped so button ends up
-    OLED::drawArea(42, 0, 42, 16, buttonB); // on right side of screen
-    OLED::setCursor(84, 0);
-    ui_draw_power_source_icon();
-  }
-  tipDisconnectedDisplay = false;
-  if (tipTemp > 55) {
-    tempOnDisplay = true;
-  } else if (tipTemp < 45) {
-    tempOnDisplay = false;
-  }
-  if (isTipDisconnected()) {
-    tempOnDisplay          = false;
-    tipDisconnectedDisplay = true;
-  }
-  if (tempOnDisplay || tipDisconnectedDisplay) {
-    // draw temp over the start soldering button
-    // Location changes on screen rotation
-    if (OLED::getRotation()) {
-      // in right handed mode we want to draw over the first part
-      OLED::fillArea(55, 0, 41, 16, 0); // clear the area for the temp
-      OLED::setCursor(56, 0);
-    } else {
-      OLED::fillArea(0, 0, 41, 16, 0); // clear the area
-      OLED::setCursor(0, 0);
-    }
-    // If we have a tip connected draw the temp, if not we leave it blank
-    if (!tipDisconnectedDisplay) {
-      // draw in the temp
-      if (!(getSettingValue(SettingsOptions::CoolingTempBlink) && (xTaskGetTickCount() % 1000 < 300))) {
-        ui_draw_tip_temperature(false, FontStyle::LARGE); // draw in the temp
-      }
-    } else {
-      // Draw in missing tip symbol
+  bool tipDisconnected   = isTipDisconnected();
+  bool displayTemp       = (tipTemp > 55) && !tipDisconnected;
+  bool isFlipped         = OLED::getRotation();
+  bool isReverse         = getSettingValue(SettingsOptions::ReverseButtonNavEnabled);
 
-      if (OLED::getRotation()) {
-        // in right handed mode we want to draw over the first part
-        OLED::drawArea(54, 0, 42, 16, disconnectedTipF);
-      } else {
-        OLED::drawArea(0, 0, 42, 16, disconnectedTip);
-      }
-    }
-  }
+    // Flip and switch buttons accordingly
+    OLED::drawArea(isFlipped ? 54: 0, 0, 42, 16, isFlipped ? (isReverse ? buttonBF:buttonAF) : (isReverse ? buttonB:buttonA) );
+	OLED::drawArea(isFlipped ? 12:42, 0, 42, 16, isFlipped ? (isReverse ? buttonAF:buttonBF) : (isReverse ? buttonA:buttonB) );
+	
+	if (displayTemp || tipDisconnected) {
+		// draw temp over the start soldering button
+		// Location changes on screen rotation
+		  // in right handed mode we want to draw over the first part
+		  OLED::fillArea(isReverse? (isFlipped?14:42):(isFlipped?55:0), 0, 41, 16, 0); // clear the area
+		  OLED::setCursor(isReverse? (isFlipped?15:43):(isFlipped?56:0), 0);
+		// If tip is connected draw the temp, otherwise - the notification
+		if (!tipDisconnected) {
+		  // Draw-in the temp
+		  if (!(getSettingValue(SettingsOptions::CoolingTempBlink) && (xTaskGetTickCount() % 1000 < 300))) {
+			ui_draw_tip_temperature(false, FontStyle::LARGE); // draw in the temp
+		  }
+		} else {
+		  // Draw-in the missing tip symbol
+		  if (isReverse) {
+				OLED::drawArea(isFlipped?12:42, 0, 42, 16, isFlipped?disconnectedTipF:disconnectedTip);
+		  } else {
+				OLED::drawArea(isFlipped? 54:0, 0, 42, 16, isFlipped?disconnectedTipF:disconnectedTip);
+		  }
+		}
+	}
+	OLED::setCursor(isFlipped ? 0:84, 0);
+	ui_draw_power_source_icon();
 }
 
 #endif
