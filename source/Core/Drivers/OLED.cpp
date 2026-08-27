75ecc6823d0b20860f38f47340a19f5ab99390a6
Edrig <vivarais51@gmail.com>
2026-08-27 13:24:36 +0200

    TS101: remove redundant segment remap branch in OLED setup
    The OLED_DISPLAY_OFFSET_QUIRK branch duplicated the #else branch
    (both wrote 0xA0 with the same comment), so it can be dropped.

--- source/Core/Drivers/OLED.cpp
+++ source/Core/Drivers/OLED.cpp
@@ -49,11 +49,9 @@ I2C_CLASS::I2C_REG OLED_Setup_Array[] = {
 #endif
     {0x80,             0x40, 0}, /* Set Display start line to 0 */
 #if defined(OLED_SEGMENT_MAP_REVERSED) && !defined(OLED_DISPLAY_OFFSET_QUIRK)
-    {0x80,             0xA1, 0}, /* Set Segment remap to normal */
-#elif defined(OLED_DISPLAY_OFFSET_QUIRK)
-    {0x80, 0xA0, 0}, /* Set Segment remap to normal */
+    {0x80,             0xA1, 0}, /* Set Segment remap (reversed) */
 #else
-    {0x80, 0xA0, 0}, /* Set Segment remap to normal */
+    {0x80,             0xA0, 0}, /* Set Segment remap to normal */
 #endif
     {0x80,             0x8D, 0}, /* Charge Pump */
     {0x80,             0x14, 0}, /* Charge Pump settings */
